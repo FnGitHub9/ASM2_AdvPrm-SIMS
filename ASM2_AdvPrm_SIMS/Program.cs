f52ef4bba7f9547f4fb56ea46d4cb1acc63319fd
FnGitHub9 <finngamingwj@gmail.com>
2024-04-06 20:34:23 +0700

    new commit

--- ASM2_AdvPrm_SIMS/Program.cs
+++ ASM2_AdvPrm_SIMS/Program.cs
@@ -12,7 +12,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 } 
-//help me 
+//help me please im in spain but the s is silent
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
