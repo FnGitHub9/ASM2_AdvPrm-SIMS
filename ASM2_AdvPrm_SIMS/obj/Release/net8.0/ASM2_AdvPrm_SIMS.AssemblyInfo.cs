77f2835227033a0a2709b99148f0ec8c70727044
FnGitHub9 <finngamingwj@gmail.com>
2024-04-06 19:55:30 +0700

    second commit

--- ASM2_AdvPrm_SIMS/obj/Release/net8.0/ASM2_AdvPrm_SIMS.AssemblyInfo.cs
+++ ASM2_AdvPrm_SIMS/obj/Release/net8.0/ASM2_AdvPrm_SIMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASM2_AdvPrm_SIMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acfe8e837d0c84d3da0b19f77f412804cf581ac4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASM2_AdvPrm_SIMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASM2_AdvPrm_SIMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
