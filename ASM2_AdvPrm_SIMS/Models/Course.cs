5a2da3fc7d29b87566fffa9eb88f31cccb6db310
FnGitHub9 <finngamingwj@gmail.com>
2024-04-07 20:31:26 +0700

    added Course and stuffs

---
+++ ASM2_AdvPrm_SIMS/Models/Course.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASM2_AdvPrm_SIMS.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        [Required]
+        public string SubjectCode { get; set; }
+        public string Subject { get; set; }
+
+        public bool Status { get; set; }
+    }
+}
