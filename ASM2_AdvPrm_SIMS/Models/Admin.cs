86372ca9ef0973c431bed6e77f192e405cb7fd12
FnGitHub9 <finngamingwj@gmail.com>
2024-04-07 11:18:28 +0700

    overwrite random files?

--- ASM2_AdvPrm_SIMS/Models/Admin.cs
+++ ASM2_AdvPrm_SIMS/Models/Admin.cs
@@ -1,6 +1,12 @@
-﻿namespace ASM2_AdvPrm_SIMS.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASM2_AdvPrm_SIMS.Models
 {
     public class Admin
     {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string email { get; set; }
     }
 }
