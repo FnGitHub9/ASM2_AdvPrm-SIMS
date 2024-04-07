6a0198e06ff9bd7cbf21b7307177d13a4ca770f2
Jonhnathan2 <supbros847@gmail.com>
2024-04-07 17:23:06 +0700

    New

--- ASM2_AdvPrm_SIMS/Context/TeacherContext.cs
+++ ASM2_AdvPrm_SIMS/Context/TeacherContext.cs
@@ -63,7 +63,7 @@ namespace ASM2_AdvPrm_SIMS.Context
                             string FirstName = parts[1];
                             string LastName = parts[2];
                             string Subject = parts[3];
-                            int Status = int.Parse(parts[4]);
+                            bool Status = bool.Parse(parts[4]);
 
                             teachers.Add(new Teacher
                             {
--- ASM2_AdvPrm_SIMS/Models/Teacher.cs
+++ ASM2_AdvPrm_SIMS/Models/Teacher.cs
@@ -6,7 +6,10 @@ namespace ASM2_AdvPrm_SIMS.Models
     {
         public int Id { get; set; }
         [Required]
-        public string Name { get; set; }
-        public string email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Status { get; set; }
+        public string Subject { get; set; }
     }
 }
