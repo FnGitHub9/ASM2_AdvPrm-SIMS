2bdc585c3f50839839354b7ea1b7257720de1d34
daquick2312 <daquick2312@gmail.com>
2024-04-08 22:00:33 +0700

    Create Student Management page

--- ASM2_AdvPrm_SIMS/Pages/Dashboard.cshtml.cs
+++ ASM2_AdvPrm_SIMS/Pages/Dashboard.cshtml.cs
@@ -1,12 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ASM2_AdvPrm_SIMS.Context;
+using ASM2_AdvPrm_SIMS.Service;
+using ASM2_AdvPrm_SIMS.Models;
 
 namespace ASM2_AdvPrm_SIMS.Pages
 {
     public class DashboardModel : PageModel
-    {
+    {/*
+        private readonly CourseService _courseService;
+
+        public IList<Course> Courses { get; set; } = default!;
+
+        public Course NewCourse { get; set; } = default!;
+
+        public DashboardModel(CourseService service)
+        {
+            _courseService = service;
+        }
         public void OnGet()
         {
-        }
+            Courses = _courseService.GetCourses();
+        }*/
     }
 }
