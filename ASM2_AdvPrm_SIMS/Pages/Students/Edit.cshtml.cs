65a2ba33fb9617e9ab8badfcaa4fc68ed30bb200
daquick2312 <daquick2312@gmail.com>
2024-04-09 15:25:02 +0700

    Unit Test

--- ASM2_AdvPrm_SIMS/Pages/Students/Edit.cshtml.cs
+++ ASM2_AdvPrm_SIMS/Pages/Students/Edit.cshtml.cs
@@ -1,12 +1,45 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ASM2_AdvPrm_SIMS.Models;
+using ASM2_AdvPrm_SIMS.Service;
 
 namespace ASM2_AdvPrm_SIMS.Pages.Students
 {
     public class EditModel : PageModel
     {
-        public void OnGet()
+        private readonly StudentService _service;
+
+        public EditModel(StudentService service)
         {
+            _service = service;
+        }
+
+        [BindProperty]
+        public Student Students { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? itemid)
+        {
+            if (itemid == null)
+            {
+                return NotFound();
+            }
+            var pizza = _service.Students.FirstOrDefault(p => p.Id == itemid);
+            if (pizza == null)
+            {
+                return NotFound();
+            }
+            Students = pizza;
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            _service.UpdateStudent(Students.Id, Students);
+            return RedirectToPage(nameof(Index));
         }
     }
 }
---
+++ SIMS-Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace SIMS_Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
